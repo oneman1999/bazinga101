921a623b1b9237fb790bfa46b4b6e4d6bdd794b0
oneman1999 <alexvlahos2@gmail.com>
2017-09-16 04:51:13 +0300

    JFrame , Canvas , Graphics

--- ProjectStargame/src/com/TeamName/GameTitle/Game.java
+++ ProjectStargame/src/com/TeamName/GameTitle/Game.java
@@ -1,16 +1,94 @@
 package com.TeamName.GameTitle;
 
-public class Game implements Runnable{
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
-	public Game() {
+import javax.swing.text.StyledEditorKit.FontSizeAction;
+
+import com.TeamName.GameTitle.display.Display;
+
+public class Game implements Runnable {
+
+	private Display display;
+	public int width, height;
+	public String title;
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	private BufferStrategy bs;
+	private Graphics g;
+	
+	public Game(String title, int width, int height){
+		this.width = width;
+		this.height = height;
+		this.title = title;
+	}
+	
+	private void init(){
+		display = new Display(title, width, height);
+	}
+	
+	private void tick(){
 		
 	}
 	
+	private void render(){
+		bs = display.getCanvas().getBufferStrategy();
+		if(bs == null){
+			display.getCanvas().createBufferStrategy(3);
+			return;
+		}
+		g = bs.getDrawGraphics();
+		//Draw Here!
+		{
+			
+			g.setColor(Color.BLUE);
+			g.fillRect(0, 0, width, height);
+			g.setColor(Color.GREEN);
+			g.setFont(new Font("DialogInput" , 3 , 58));
+			g.drawString("WELCOME FELLOW TIME TRAVELLERS", width/5 , height/3);
+			
+		}	
+		//End Drawing!
+		bs.show();
+		g.dispose();
+	}
 	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	
+	public void run(){
+		
+		init();
+		
+		//Game Loop
+		while(running){
+			tick();
+			render();
+		}
+		
+		stop();
 		
 	}
-
+	
+	public synchronized void start(){
+		if(running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		if(!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
--- ProjectStargame/src/com/TeamName/GameTitle/Launcher.java
+++ ProjectStargame/src/com/TeamName/GameTitle/Launcher.java
@@ -1,9 +1,17 @@
 package com.TeamName.GameTitle;
 
+import java.awt.GraphicsEnvironment;
+
 public class Launcher {
 	
-	public static void main(Stringp[] args) {
-		new Game();
+	private static final float SCALE = 0.9F;
+
+	public static void main(String[] args){
+		Game game = new Game("Stargame",
+				 Math.round(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width * SCALE) ,
+				 Math.round(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height * SCALE));
+		
+		game.start();
 	}
 	
 }
