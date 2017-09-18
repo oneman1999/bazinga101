c880ad9b4ab3bdd46ba8959049bbabee174e87bf
oneman1999 <alexvlahos2@gmail.com>
2017-09-18 06:13:14 +0300

    ...

---
+++ ProjectStargame/src/com/TeamName/GameTitle/display/graphics/Assets.java
@@ -0,0 +1,25 @@
+package com.TeamName.GameTitle.display.graphics;
+
+import java.awt.image.BufferedImage;
+
+public class Assets {
+
+	//Declaring the sprites
+	public static BufferedImage sprite0;
+	public static BufferedImage sprite1;
+	
+	
+	
+	public static void loadSpriteSheets(){
+		
+		//First spritesheet
+		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("") , 4 , 4);
+		
+		//Initializing the images for the first spritesheet
+		sprite0 = sheet.getSpriteWithCoords(1, 1);
+		sprite1 = sheet.getSpriteWithCoords(2, 1);
+		
+	}
+	
+	
+}
---
+++ ProjectStargame/src/com/TeamName/GameTitle/display/graphics/SpriteSheet.java
@@ -0,0 +1,95 @@
+package com.TeamName.GameTitle.display.graphics;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteSheet {
+
+	private BufferedImage sheet;
+	
+	private int width , height , imageWidth , imageHeight , spriteWidth , spriteHeight;
+	
+	
+	public SpriteSheet(BufferedImage SpriteSheet , int SpritesPerLine , int SpritesPerColumn) {
+		this.sheet = SpriteSheet;
+		this.width = SpritesPerLine;
+		this.height = SpritesPerColumn;
+		this.imageWidth = sheet.getWidth();
+		this.imageHeight = sheet.getHeight();
+		this.spriteWidth = imageWidth / width;
+		this.spriteHeight = imageHeight / height;
+	}
+	
+	/**
+	 * 
+	 * @param Line the sprite is in
+	 * @param Column the sprite is in
+	 * @return The specified sprite as a BufferedImage
+	 */
+	public BufferedImage getSpriteWithCoords(int Line , int Column) {	
+		return sheet.getSubimage(spriteWidth*(Line - 1) , spriteHeight*(Column - 1), spriteWidth , spriteHeight);
+	}
+	
+	/**
+	 * Crops a subimage directly from coordinates given
+	 * @param x : the x coordinate of the upper left corner of the sprite (in pixels)
+	 * @param y : the y coordinate of the upper left corner of the sprite (in pixels)
+	 * @return the subimage croped from the coordinates given in pixels
+	 */
+	public BufferedImage getSpriteDirectly(int x , int y) {
+		return sheet.getSubimage(x, y, spriteWidth, spriteHeight);
+	}
+	
+	
+	public BufferedImage getSpriteSheetAsBufferedImage() {
+		return sheet;
+	}
+	
+	
+	/**
+	 * 
+	 * @return The number of Lines in the SpriteSheet
+	 */
+	public int getSheetWidth() {
+		return width;
+	}
+	
+	/**
+	 * 
+	 * @return The number of Columns in the SpriteSheet
+	 */
+	public int getSheetHeight() {
+		return height;
+	}
+	
+	/**
+	 * 
+	 * @return The width of the SpriteSheet in pixels
+	 */
+	public int getImageWidth() {
+		return imageWidth;
+	}
+	
+	/**
+	 * 
+	 * @return The height of the SpriteSheet in pixels
+	 */
+	public int getImageHeight() {
+		return imageHeight;
+	}
+	
+	/**
+	 * 
+	 * @return The width of a single sprite in pixels
+	 */
+	public int getSpriteWidth() {
+		return spriteWidth;
+	}
+	
+	/**
+	 * 
+	 * @return The height of a single sprite in pixels
+	 */
+	public int getSpriteHeight() {
+		return spriteHeight;
+	}
+}
